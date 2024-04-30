21bdbd49730634002b553f0a2ca65b2d1cc620d5
Unfortunated <120785157+Unfortunated@users.noreply.github.com>
2024-04-30 13:49:49 +0700

    Update CDLinkedList.java

--- Homework11/src/CDLinkedList.java
+++ Homework11/src/CDLinkedList.java
@@ -167,31 +167,174 @@ public class CDLinkedList {
 
 	// write the sort method below
 	public void sort() throws Exception { // any sorting will do BUT you must use only 'this' list. No array or any other data structures allowed!!!
+//		bubbleSort();
+//		selectionSort();
+//		insertionSort();
+		mergeSort();
+//		quickSort();
+		}
+	
+	public void bubbleSort() throws Exception {
+		//bubble sort
 		//check for boundary conditions
 		if (this == null || this.size == 1 || this.isEmpty()) {
 			return;
 		}
-//		//bubble sort
-//		//set up outer loop
-//		int counter = this.size-1;
-//		//outer loop
-//		while (counter != 0) {
-//			//setup inner loop
-//			DListIterator i = new DListIterator(header.nextNode);
-//			//inner loop
-//			while (i.currentNode.nextNode != header) {
-//				//swapping
-//				if (i.currentNode.data > i.currentNode.nextNode.data) {
-//					int temp = i.currentNode.data;
-//					i.currentNode.data = i.currentNode.nextNode.data;
-//					i.currentNode.nextNode.data = temp;
-//				}
-//				i.next();
-//			}
-//			counter -= 1;
-//			}
-		
-		
-		
+		//set up outer loop
+		int counter = this.size-1;
+		//outer loop
+		while (counter != 0) {
+			//setup inner loop
+			DListIterator i = new DListIterator(header.nextNode);
+			//inner loop
+			while (i.currentNode.nextNode != header) {
+				//swapping
+				if (i.currentNode.data > i.currentNode.nextNode.data) {
+					int temp = i.currentNode.data;
+					i.currentNode.data = i.currentNode.nextNode.data;
+					i.currentNode.nextNode.data = temp;
+				}
+				i.next();
+			}
+			counter -= 1;
+			}
+		//Bubble sort end
 		}
+	
+	public void selectionSort() throws Exception {
+		//Selection sort
+		//set up outer loop
+		DListNode marker = header.previousNode;
+		while (marker.previousNode != header) {
+			//set up inner loop
+			int max = marker.data;
+			//i is used to find the maximum in the unsorted portion of array
+			DListNode i = marker.previousNode;
+			//maxVal track where the maximum value is
+			DListNode maxVal = marker;
+			//inner loop to find max data in the unsorted portion of array
+			while (i != header) {
+				if (i.data > max) {
+					max = i.data;
+					maxVal = i;
+				}
+				i = i.previousNode;
+			}
+			//swap value
+			int temp = marker.data;
+			marker.data = max;
+			//place the data into the location of maxValue
+			maxVal.data = temp;
+			//move marker back
+			marker = marker.previousNode;
+			}
+		//Selection sort end
+		}
+	
+	public void insertionSort() throws Exception {
+		//Insertion sort
+		//set up outer loop
+		//marker is used to track the last element of sorted portion
+		DListNode marker = header.nextNode;
+		while (marker != header) {
+			//set up inner loop
+			int temp = marker.data;
+			//set i as the rightmost element of sorted portion
+			DListNode i = marker;
+			//inner loop
+			while (i.previousNode != header && i.previousNode.data > temp) {
+				//copy previous data to current one until previous data is less than temp
+				i.data = i.previousNode.data;
+				i = i.previousNode;
+			}
+			//place the data
+			i.data = temp;
+			marker = marker.nextNode;
+			}
+		//Insertion sort end
+		}
+	
+	public void mergeSort() throws Exception {
+		//Merge sort
+		header = mergeSort(header);
+		}
+	
+	
+	public DListNode split(DListNode node) {
+		if (node == null || node.nextNode == header || this.size == 1) {
+			return node;
+		}
+		int mid = Math.round(size/2);
+		System.out.println(mid);
+	    DListNode left = node;
+	    DListNode right = node;
+	    
+	    // Find the middle node
+	    for (int i = 0; i < mid; i++) {
+	        right = right.nextNode;
+	    }
+	    
+	    left.previousNode = null;
+	    right.previousNode.nextNode = null;
+	    return right;
+	}
+	//recursive function for mergeSort
+	public DListNode mergeSort(DListNode node) {
+		if (node == null || node.nextNode == header || this.size == 1) {
+			return node;
+		}
+//		int middle =  Math.round((this.size-1)/2);
+//		DListNode left = mergeSort(node);
+//		DListNode right = node;
+//		while (middle != 0) {
+//			right = right.nextNode;
+//			middle -= 1;
+//		}
+//		DListNode nextToMiddle = right.nextNode;
+//		right.nextNode = null;
+//		nextToMiddle.previousNode = null;
+//		right = nextToMiddle;
+//		
+//		return merge(left,right);
+		
+	    
+	    // Split the list
+	    DListNode left = mergeSort(node);
+	    DListNode right = mergeSort(split(node));
+	    
+	    return merge(left, right);
+		}
+	
+	public DListNode merge(DListNode left, DListNode right) {
+		System.out.println("Hello mom");
+		if (left == null) {
+			return left;
+		}
+		if (right == null) {
+			return right;
+		}
+		DListNode result = null;
+		if (left.data < right.data) {
+			result = left;
+			result.nextNode.previousNode = result;
+			result.nextNode = merge(left.nextNode,right);
+			return result;
+		} else {
+			result = right;
+			result.nextNode.previousNode = result;
+			result.nextNode = merge(left,right.nextNode);
+			return result;
+		}
+	}
+	
+	public void quickSort() throws Exception {
+		if (this == null || this.size <= 1) {
+			return;
+		} else if (this.size <= 20) {
+//			this.insertionSort();
+//			this.mergeSort();
+		} else {
+			
+		}
+	}
 	}
